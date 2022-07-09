c09d3ee39bb0e887d4e3580ef887a185cc6dcaf6
jguzman1919 <joelguzman1919@gmail.com>
2022-07-09 19:26:12 -0400

    se agrega el proyecto

---
+++ CapaDatos/CD_Usuarios.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaEntidad;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace CapaDatos
+{
+    public class CD_Usuarios
+    {
+
+        public List<Usuario> Listar()
+        {
+            List<Usuario> Lista = new List<Usuario>();
+
+            try
+            {
+                using (SqlConnection oConexion= new SqlConnection(Conexion.cn))
+
+                {
+                    string query = "select IdUsuario,Nombres,Apellidos,Correo,Clave,Reestablecer,Activo,FechaRegistro from Usuario";
+
+                    SqlCommand cmd= new SqlCommand (query,oConexion);
+                    cmd.CommandType = CommandType.Text;
+
+                    oConexion.Open();
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                            Lista.Add(
+                                new Usuario()
+                                {
+
+                                    IdUsuario = Convert.ToInt32(dr["IdUsuario"]),
+                                    Nombres = dr["Nombres"].ToString(),
+                                    Apellidos = dr["Apellidos"].ToString(),
+                                    Correo = dr["Correo"].ToString(),
+                                    Clave = dr["Clave"].ToString(),
+                                    Reestablecer = Convert.ToBoolean(dr["Reestablecer"]),
+                                    Activo = Convert.ToBoolean(dr ["Reestablecer"]),
+
+                                }
+                                
+                              );
+                        }
+                    }
+                }
+
+            }
+
+            catch
+            {
+                Lista = new List<Usuario>();
+
+            }
+
+
+            return Lista;
+        }
+        public  int Registrar(Usuario obj, out string mensaje)
+        {
+            int idautoenrolado = 0;
+            mensaje = string.Empty;
+            try
+            {
+
+                using(SqlConnection oConnexion = new SqlConnection(Conexion.cn))
+
+                {
+                    SqlCommand cmd = new SqlCommand("sp_RegistrarUsuario", oConnexion);
+                    cmd.Parameters.AddWithValue("Nombres", obj.Nombres);
+                    cmd.Parameters.AddWithValue("Apellidos", obj.Apellidos);
+                    cmd.Parameters.AddWithValue("Correo", obj.Correo);
+                    cmd.Parameters.AddWithValue("Clave", obj.Clave);
+                    cmd.Parameters.AddWithValue("Activo", obj.Activo);
+                    cmd.Parameters.Add("Resultado",SqlDbType.Int).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar,500).Direction = ParameterDirection.Output;
+                    //cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500);
+ 
+                     cmd.CommandType = CommandType.StoredProcedure;
+
+                    oConnexion.Open();
+
+                    cmd.ExecuteNonQuery();
+
+                    idautoenrolado= Convert.ToInt32(cmd.Parameters["Resultado"].Value);
+                    mensaje= cmd.Parameters["Mensaje"].Value.ToString();
+
+
+                }
+
+
+            }
+
+            catch (Exception ex)
+            {
+                idautoenrolado = 0;
+
+                mensaje= ex.Message;
+
+            }
+
+            return idautoenrolado;
+        }
+        public bool Editar(Usuario obj, out string mensaje)
+        {
+            bool resultado = false;
+            mensaje = string.Empty;
+            try
+            {
+
+                using (SqlConnection oConexion = new SqlConnection(Conexion.cn))
+
+                {
+                    SqlCommand cmd = new SqlCommand("sp_EditarUsuario", oConexion);
+                    cmd.Parameters.AddWithValue("IdUsuario", obj.IdUsuario);
+                    cmd.Parameters.AddWithValue("Nombres", obj.Nombres);
+                    cmd.Parameters.AddWithValue("Apellidos", obj.Apellidos);
+                    cmd.Parameters.AddWithValue("Correo", obj.Correo);
+                    cmd.Parameters.AddWithValue("Activo", obj.Activo);
+                    cmd.Parameters.Add("Resultado", SqlDbType.Bit).Direction = ParameterDirection.Output;
+                    cmd.Parameters.Add("Mensaje", SqlDbType.VarChar, 500).Direction = ParameterDirection.Output;
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    oConexion.Open();
+
+                    cmd.ExecuteNonQuery();
+
+                    resultado = Convert.ToBoolean(cmd.Parameters["Resultado"].Value);
+                    mensaje = cmd.Parameters["Mensaje"].Value.ToString();
+
+                }
+
+            }
+
+            catch (Exception ex)
+            {
+                resultado = false;
+                mensaje = ex.Message;
+
+            }
+
+            return resultado;
+        }
+
+        public bool Eliminar (int id, out string mensaje)
+        {
+
+            bool resultado = false;
+            mensaje = string.Empty;
+            try
+            {
+                using (SqlConnection oconexion = new SqlConnection(Conexion.cn))
+                {
+                        SqlCommand cmd = new SqlCommand("delete top (1) from usuario where IdUsuario=@id", oconexion);
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.CommandType = CommandType.Text;
+                    oconexion.Open();
+                    resultado = cmd.ExecuteNonQuery() > 0 ? true : false;
+                }
+            }
+
+            catch (Exception ex)
+            {
+                resultado=false;
+                mensaje=ex.Message;
+            }
+
+
+            return (resultado);
+
+
+
+        }
+    }
+}
---
+++ CapaNegocio/CN_Usuarios.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaDatos;
+using CapaEntidad;
+
+namespace CapaNegocio
+{
+    public class CN_Usuarios
+    {
+        private CD_Usuarios objCapaDato = new CD_Usuarios();
+
+        public List<Usuario> Listar()
+        {
+
+            return objCapaDato.Listar();
+        }
+
+        public int Registrar(Usuario obj, out string mensaje)
+        {
+            mensaje = String.Empty;
+
+            if (string.IsNullOrEmpty(obj.Nombres) || string.IsNullOrWhiteSpace(obj.Nombres))
+            {
+                mensaje = "El nombre del usuario no puede ser vacio";
+            }
+
+            else if (string.IsNullOrEmpty(obj.Apellidos) || string.IsNullOrWhiteSpace(obj.Apellidos))
+            {
+                mensaje = "El Apellidos del usuario no puede ser vacio";
+
+            }
+            else if (string.IsNullOrEmpty(obj.Correo) || string.IsNullOrWhiteSpace(obj.Correo))
+            {
+                mensaje = "El Apellidos del usuario no puede ser vacio";
+            }
+
+            if (string.IsNullOrEmpty(mensaje))
+            {
+                string Clave = "test123";
+                obj.Clave = CN_Recursos.ConvertirSha256(Clave);
+
+                return objCapaDato.Registrar(obj, out mensaje);
+            }
+
+            else
+            {
+                return 0;
+            }
+        }
+
+        public bool Editar(Usuario obj, out string mensaje)
+        {
+            mensaje = String.Empty;
+
+            if (string.IsNullOrEmpty(obj.Nombres) || string.IsNullOrWhiteSpace(obj.Nombres))
+            {
+                mensaje = "El Nombre del usuario no puede ser vacio";
+            }
+
+            else if (string.IsNullOrEmpty(obj.Apellidos) || string.IsNullOrWhiteSpace(obj.Apellidos))
+            {
+                mensaje = "El Apellido del usuario no puede ser vacio";
+
+            }
+            else if (string.IsNullOrEmpty(obj.Correo) || string.IsNullOrWhiteSpace(obj.Correo))
+            {
+                mensaje = "El Correo del usuario no puede ser vacio";
+            }
+
+            if (!string.IsNullOrEmpty(obj.Nombres))
+            {
+                return objCapaDato.Editar(obj, out mensaje);
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool Eliminar(int id, out string mensaje)
+        {
+            return objCapaDato.Eliminar(id, out mensaje);
+        }
+    }
+
+
+}
